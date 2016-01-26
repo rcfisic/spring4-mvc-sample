a5fbceb935d691dcde2d0c1a2d92a3ef703b37e0
rcfisic <rcm1986@gmail.com>
2016-01-26 23:35:10 +0100

    Sample configuration

---
+++ src/main/java/com/rcfisic/web/config/AppConfig.java
@@ -0,0 +1,15 @@
+package com.rcfisic.web.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Application context.
+ * @author rcfisic
+ *
+ */
+@Configuration
+@ComponentScan("com.rcfisic.web.service")
+public class AppConfig {
+
+}
---
+++ src/main/java/com/rcfisic/web/config/DispatcherConfig.java
@@ -0,0 +1,28 @@
+package com.rcfisic.web.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.JstlView;
+import org.springframework.web.servlet.view.UrlBasedViewResolver;
+
+/**
+ * MVC context.
+ * @author rcfisic
+ *
+ */
+@Configuration
+@EnableWebMvc
+@ComponentScan("com.rcfisic.web.controller")
+public class DispatcherConfig {
+
+	@Bean
+	public UrlBasedViewResolver setupViewResolver() {
+		UrlBasedViewResolver resolver = new UrlBasedViewResolver();
+		resolver.setPrefix("/WEB-INF/views/");
+		resolver.setSuffix(".jsp");
+		resolver.setViewClass(JstlView.class);
+		return resolver;
+	}
+}
